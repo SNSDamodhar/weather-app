165d39c37411108767966eb363f21083d756ebb1
damodhar <sadhudamodhar12@gmail.com>
2020-10-18 18:48:58 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+import { async } from "q";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//it is the API_KEY from open weather site
+const API_KEY = "bea188da77ccaddf3999efee269d5738";
+
+
+class App extends React.Component
+{
+
+  //We are maintaining the state for various feilds 
+  state = {
+    temprature : undefined,
+    city : undefined,
+    country : undefined,
+    humidity : undefined,
+    description : undefined,
+    error : undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    //We are getting values from the JSON object which is came from weather website through API call
+    const city  = e.target.elements.city.value;
+    const country  = e.target.elements.country.value;
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    
+    if(city && country)
+    {
+      console.log(data);
+
+      this.setState({
+        temprature : data.main.temp,
+        city : data.name,
+        country : data.sys.country,
+        humidity : data.main.humidity,
+        description : data.weather[0].description,
+        error : ""
+      })
+    }
+    else
+    {
+      console.log(data);
+
+      this.setState({
+        temprature : undefined,
+        city : undefined,
+        country : undefined,
+        humidity : undefined,
+        description : undefined,
+        error : "Please Enter the Value"
+      })
+    }
+  }
+  
+  //render() is used to display content to the webpage
+  //render() should return only one parent element
+  render(){
+
+    return(
+      <div>
+        <div className = "wrapper">
+          <div className = "main">
+            <div className = "container">
+              <div className = "row">
+                <div className = "col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className = "col-xs-7 form-container">
+                  {/* this is comment in jsx 
+                    We are passing function as props to Form Component*/ }
+                  <Form getWeather = {this.getWeather}/>
+                  {/*We are passing props to Weather Component */}
+                  <Weather 
+                    temprature = {this.state.temprature}
+                    city = {this.state.city}
+                    country = {this.state.country}
+                    humidity = {this.state.humidity}
+                    description = {this.state.description}
+                    error = {this.state.error}
+                   />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+
+        
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+//importing our component
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//First parameter represents component name
+//Second parameter represents where we have to insert that component
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
